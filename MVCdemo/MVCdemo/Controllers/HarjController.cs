88f6c6fef681d74c08024ab932b3e058e76417ec
Sanna Wilson <sanna.a.wilson@gmail.com>
2019-09-18 14:40:24 +0300

    Harj-näkymän editointia; kontrollerilla alisivu Welcome, jossa osoiterivin kanssa kommunikoiva sisältö.

--- MVCdemo/MVCdemo/Controllers/HarjController.cs
+++ MVCdemo/MVCdemo/Controllers/HarjController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace MVCdemo.Controllers
 {
@@ -11,6 +12,10 @@ namespace MVCdemo.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hei {name}, osoiterivissa on numero: {numTimes}");
         }
     }
 }
