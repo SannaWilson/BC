caf01ebf70b0a39b6b8cbfb75fbdac0619f5729d
Sanna Wilson <sanna.a.wilson@gmail.com>
2019-09-18 11:06:12 +0300

    Loin harjoitus-kontrollerin ja sivupohjan

---
+++ MVCdemo/MVCdemo/Controllers/HarjController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCdemo.Controllers
+{
+    public class HarjController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
