7f0f21309a5d6e7a6e697334a081dc762ffcbbee
Sanna Wilson <sanna.a.wilson@gmail.com>
2019-09-30 09:21:00 +0300

    Tarkistus. if-else -lausekkeen käyttö.

--- CsharpTreenia/CsharpTreenia/Program.cs
+++ CsharpTreenia/CsharpTreenia/Program.cs
@@ -9,15 +9,27 @@ namespace CsharpTreenia
             /* Tehdään peli, joka kehittyy vaiheittain:
              * 1) Lisätään ajanotto olemassa olevaan harjoitukseen
              * 2) Muokataan tehtävää ja lisätään vastaukselle pituusehto.
+             * 3) Tarkistus, onko pituusehto täyttynyt.
             */
                 
             Console.WriteLine("Kirjoita vähintään 10 merkin pätkä.");
+
             DateTime alku = DateTime.Now;
             String vastaus = Console.ReadLine();
+
             int pituus = vastaus.Length;
             DateTime loppu = DateTime.Now;
             TimeSpan aika = loppu - alku;
-            Console.WriteLine("Vastausaikasi oli: " + aika + ". Kirjoitit: " + pituus + " merkkiä.");
+
+            if (pituus >9)
+            {
+                Console.WriteLine("Vastausaikasi oli: " + aika + ". Kirjoitit: " + pituus + " merkkiä.");
+            }
+            else
+            {
+                Console.WriteLine("Vastaus oli liian lyhyt.");
+            }
+            
         }
     }
 }
