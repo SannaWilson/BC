0ed48e51b4ee106b9c520e2b5eab98afe8a658eb
Sanna Wilson <sanna.a.wilson@gmail.com>
2019-09-30 10:18:27 +0300

    Järjstelyä luokkiin.

--- CsharpTreenia/CsharpTreenia/Program.cs
+++ CsharpTreenia/CsharpTreenia/Program.cs
@@ -1,27 +1,31 @@
 ﻿using System;
-
+            /* Tehdään peli, joka kehittyy vaiheittain:
+             * 1) Lisätään ajanotto olemassa olevaan harjoitukseen
+             * 2) Muokataan tehtävää ja lisätään vastaukselle pituusehto.
+             * 3) Tarkistus, onko pituusehto täyttynyt.
+             * 4) Selkiytetään koodia ja järjestellään luokkia
+            */
 namespace CsharpTreenia
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /* Tehdään peli, joka kehittyy vaiheittain:
-             * 1) Lisätään ajanotto olemassa olevaan harjoitukseen
-             * 2) Muokataan tehtävää ja lisätään vastaukselle pituusehto.
-             * 3) Tarkistus, onko pituusehto täyttynyt.
-            */
-                
+            Peli.Tehtävä();
+        }
+    }
+    public static class Peli
+    {
+        public static void Tehtävä()
+        {
             Console.WriteLine("Kirjoita vähintään 10 merkin pätkä.");
 
             DateTime alku = DateTime.Now;
             String vastaus = Console.ReadLine();
-
-            int pituus = vastaus.Length;
             DateTime loppu = DateTime.Now;
+            int pituus = vastaus.Length;
             TimeSpan aika = loppu - alku;
-
-            if (pituus >9)
+            if (pituus > 9)
             {
                 Console.WriteLine("Vastausaikasi oli: " + aika + ". Kirjoitit: " + pituus + " merkkiä.");
             }
@@ -29,7 +33,6 @@ namespace CsharpTreenia
             {
                 Console.WriteLine("Vastaus oli liian lyhyt.");
             }
-            
         }
     }
 }
