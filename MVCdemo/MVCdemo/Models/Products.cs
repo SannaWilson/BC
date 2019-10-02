76612573423eebc08a1532a42d7943bebe9f58b1
Sanna Wilson <sanna.a.wilson@gmail.com>
2019-10-02 14:37:53 +0300

    Pari yritystä yhdistää SQL server express -tietokantaan. Jombotron-muokkailua.

--- MVCdemo/MVCdemo/Models/Products.cs
+++ MVCdemo/MVCdemo/Models/Products.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCdemo.Models
 {
