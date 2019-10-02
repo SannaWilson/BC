2aebb3e76381ad70a8897794a345099a63241bc2
Sanna Wilson <sanna.a.wilson@gmail.com>
2019-10-02 15:17:00 +0300

    Kokonaisuuden parantelua sujuvammaksi.

--- CsharpTreenia/CsharpTreenia/Program.cs
+++ CsharpTreenia/CsharpTreenia/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Linq;
-/* Tehdään peli, joka kehittyy vaiheittain:
-* 1) Lisätään ajanotto olemassa olevaan harjoitukseen
-* 2) Muokataan tehtävää ja lisätään vastaukselle pituusehto.
-* 3) Tarkistus, onko pituusehto täyttynyt.
-* 4) Selkiytetään koodia ja järjestellään luokkia
-* 5) Luodaan lisää osia, että saadaan koodiin lisää sisältöä: Aloitus
-* 6) Ohjauslauseharjoituksia Tehtävissä 1-3:
+/* Tässä harjoituksessa on tehty seuraavia asioita:
+* 1) Lisätty ajanotto harjoitukseen
+* 2) Määritelty vastaukselle pituusehto.
+* 3) Tarkistus, onko pituusehto täyttyy.
+* 4) Käytetty luokkia ohjelman rakenteen pohjana:
+*    1) Aloitus
+*    2) Interaktiivinen osuus
+*    3) Lopetus
+* 6) Käytetty ohjauslauseita:
 *      1) if...else
 *      2) switch...case
 *      3) do..while
@@ -22,14 +24,21 @@ namespace CsharpTreenia
         static void Main(string[] args)
         {
             Peli.Aloitus();
+            Console.WriteLine("Tässä ohjelmassa on kuusi erilaista tehtävää. Voit valita minkä tahansa tehtävän ja jatkaa niin kauan kuin haluat.");
+
             Peli.Tehtävä7();
-            Console.WriteLine("Haluatko testata toisen tehtävän? Paina 1=Kyllä tai 2=Ei.");
-            String jatkuuko = Console.ReadLine();
-            int j = int.Parse(jatkuuko);
-            if (j==1)
+
+            string jatkuuko = "K";
+            do
             {
+            Console.WriteLine("\nHaluatko jatkaa? Kirjoita K=Kyllä tai E=Ei.");
+                jatkuuko = Console.ReadLine();
+                if (jatkuuko=="K")
+                {
                 Peli.Tehtävä7();
-            }
+                }
+            } while (jatkuuko=="K");
+
             Peli.Lopetus();
         }
     }
@@ -49,7 +58,7 @@ namespace CsharpTreenia
         }
         public static void Lopetus()
         {
-            Console.Write("Hauskaa oli; paina mitä tahansa näppäintä.");
+            Console.Write("Hauskaa oli! Kiitos, että pelasit.\n\n");
             Console.ReadKey();
         }
         public static void Tehtävä()
@@ -73,7 +82,7 @@ namespace CsharpTreenia
         public static void Tehtävä2()
         {
             // switch...case
-            Console.WriteLine("Kokeillaan seuraavaksi, onko sinulla onnea. Heitä noppaa painamalla jotain näppäintä.");
+            Console.WriteLine("Kokeillaan, onko sinulla onnea. Heitä noppaa painamalla jotain näppäintä.");
             Console.ReadKey();
             Random Noppa = new Random();
             int arpa = Noppa.Next(1, 7);
@@ -100,7 +109,7 @@ namespace CsharpTreenia
         public static void Tehtävä3()
         {
             // do...while
-            Console.WriteLine("Heitä noppaa, kunnes saat kuutosen.");
+            Console.WriteLine("Heitetään noppaa, kunnes saat kuutosen.");
                 Console.ReadKey();
             int arpa2 = 0;
             do
@@ -117,16 +126,16 @@ namespace CsharpTreenia
         public static void Tehtävä4()
         {
             string[] tiimi = { "Sanna", "Jennina", "Anne", "Kirsi", "Cathy" };
-
+            Console.WriteLine("BootCamp Mimmit tiimiin kuuluvat:");
             foreach (string jäsen in tiimi)
             {
-                Console.WriteLine("Hei, " + jäsen + "!");
+                Console.WriteLine(jäsen + "!");
             }
         }
         public static void Tehtävä5()
         {
             string[] tiimi = { "Sanna", "Jennina", "Anne", "Kirsi", "Cathy" };
-            Console.WriteLine("Hae tiimin jäseniä nimellä:");
+            Console.WriteLine("Hae BootCamp Mimmit -tiimin jäseniä nimellä:");
             string haku = Console.ReadLine();
 
             if (tiimi.Contains(haku))
@@ -140,7 +149,7 @@ namespace CsharpTreenia
             Console.WriteLine(haku + " löytyi listasta.");
 
             Lopetus:
-            Console.WriteLine("Jatketaan tästä...");
+            Console.WriteLine("Voit tehdä uuden haun valitsemalla tehtävän 5 uudestaan.");
         }
         static int Noppa3()
         {
@@ -150,14 +159,14 @@ namespace CsharpTreenia
         }
         public static void Tehtävä6()
         {
-            Console.WriteLine("Paina mitä tahansa näppäintä heittääksesi taas arpaa.");
+            Console.WriteLine("Paina mitä tahansa näppäintä heittääksesi arpaa.");
             Console.ReadKey();
             int numero = Noppa3();
             Console.WriteLine("Sait numeron: " + numero);
         }
         public static void Tehtävä7()
         {
-            Console.WriteLine("\n\n Tässä ohjelmassa on kuusi erilaista toimintoa tai tehtävää. Valitse numero 1-6 ja aloitetaan!");
+            Console.WriteLine("\n\n Valitse seuraavan tehtävän numero (1-6):");
 
             string syöte = Console.ReadLine();
             int tehtNro = int.Parse(syöte);
