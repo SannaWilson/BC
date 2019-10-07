c1f42e5fe1a047ee6327eee4d9f56410dc75d8e1
Sanna Wilson <sanna.a.wilson@gmail.com>
2019-10-07 13:38:34 +0300

    Asiakaslistaus tietokannasta ja http-attribuuttien kanssa halutun pyynnön määrittäminen haluttuun metodiin

--- SQL_BCharj/SQL_BCharj/Controllers/CustomersController.cs
+++ SQL_BCharj/SQL_BCharj/Controllers/CustomersController.cs
@@ -4,67 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SQL_BCharj.Models;
 
 namespace SQL_BCharj.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class Auto
-    {
-        public string Väri { get; set; }
-        public int Huippunopeus { get; set; }
-    }
 
     public class CustomersController : ControllerBase
     {
-        [Route("")] // Periaatteessa tarpeeton; koodin selkeyden vuoksi hyvä olla 
-        public int Oletus()
+        [HttpGet]
+        [Route("")]
+        public List<Customers> ListaaKaikki()
         {
-            return 123;
+            NorthwindContext ohjain = new NorthwindContext();
+            List<Customers> kaikki = ohjain.Customers.ToList();
+            return kaikki;
         }
-        [Route("table")]
-        public int[] Luvut()
+        [Route("")]
+        [HttpDelete]
+        public bool PoistaAsiakkaat()
         {
-            int[] luvut = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            return luvut;
-        }
-        [Route("1")] 
-        public string Tervehdys()
-        {
-            return "Moikka!";
-        }
-        [Route("2")]
-        public string Tervehdys2()
-        {
-            return "Hei!";
-        }
-        // Etherpadistä kopioidut "testi"-metodit
-        [Route("testi1")]
-        public int Luku()
-        {
-            return 12345;
-        }
-
-        [Route("testi2")]
-        public int[] Numerot()
-        {
-            int[] luvut = { 1, 2, 3, 4, 5 };
-            return luvut;
-        }
-
-        [Route("testi3")]
-        public bool Boolean()
-        {
-            return true;
-        }
-
-        [Route("testi4")]
-        public Auto Luokka()
-        {
-            Auto a = new Auto();
-            a.Väri = "Keltainen";
-            a.Huippunopeus = 245;
-            return a;
+            return false;
         }
     }
 }
